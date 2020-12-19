c59bed33df0183aae91f317923dcce30abc36e80
Prajakta Purohit <prajakta@chef.io>
2020-12-19 00:09:04 -0800

    Update postgres default version to 12
    Signed-off-by: Prajakta Purohit <prajakta@chef.io>

--- omnibus/files/private-chef-cookbooks/private-chef/attributes/default.rb
+++ omnibus/files/private-chef-cookbooks/private-chef/attributes/default.rb
@@ -207,7 +207,7 @@ elasticsearch['jvm_opts'] = []
 # heap address: 0x00000000c0000000, size: 28672 MB, Compressed Oops mode: Zero based, Oop shift amount: 3
 #
 # ... turns up that our limit is around 28G, which is where the max
-# should be set. 
+# should be set.
 #
 #
 elasticsearch['heap_size'] = Elasticsearch.heap_size_default(node)
@@ -517,7 +517,7 @@ default['private_chef']['nginx']['show_welcome_page'] = true
 # PostgreSQL
 ###
 # For now, we're hardcoding the version directory suffix here:
-default['private_chef']['postgresql']['version'] = '9.6'
+default['private_chef']['postgresql']['version'] = '12.0'
 # In the future, we're probably going to want to do something more elegant so we
 # don't accidentally overwrite this directory if we upgrade PG to 9.3: keeping these
 # directories straight is important because in the distant future (the year 2000)
